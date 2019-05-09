3dea6c4a5b9c1f16e9ad5d512f6d8aa197f74842
Derek <zhaomingbo1010@126.com>
2019-05-09 20:25:12 +0800

    Add project files.

---
+++ DotNetMonitor64/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetMonitor64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
