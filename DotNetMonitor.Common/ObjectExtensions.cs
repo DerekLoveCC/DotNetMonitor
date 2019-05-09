2bb8be4f5fec084732d4293d6a6ed6f23adc23bc
Derek <zhaomingbo1010@126.com>
2019-05-09 20:56:12 +0800

    add more files.

---
+++ DotNetMonitor.Common/ObjectExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace DotNetMonitor.Common
+{
+    public static class ObjectExtensions
+    {
+        public static int? ToNullableInt(this object obj)
+        {
+            if (obj == null)
+            {
+                return null;
+            }
+
+            if (int.TryParse(obj.ToString(), out int result))
+            {
+                return result;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
--- DotNetMonitor64/Program.cs
+++ DotNetMonitor64/Program.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using DotNetMonitor.Common;
+using Microsoft.Diagnostics.Runtime;
+using System.Diagnostics;
+using System.Linq;
 
 namespace DotNetMonitor64
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var processId = args[0].ToNullableInt();
+            if (processId == null)
+            {
+                processId = Process.GetProcessesByName(args[0]).FirstOrDefault()?.Id;
+            }
+            using (DataTarget dataTarget = DataTarget.AttachToProcess(processId.Value, 500, AttachFlag.NonInvasive))
+            {
+                foreach (var clrVersion in dataTarget.ClrVersions)
+                {
+                    var runTime = clrVersion.CreateRuntime();
+                }
+            }
         }
     }
 }
