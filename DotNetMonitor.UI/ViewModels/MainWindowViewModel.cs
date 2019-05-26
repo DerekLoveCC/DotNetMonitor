c1391b53726561c33c65a1916a0754c9f6a0bc57
Derek <zhaomingbo1010@126.com>
2019-05-27 07:46:25 +0800

    add process view

---
+++ DotNetMonitor.UI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,60 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DotNetMonitor.UI.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public MainWindowViewModel()
+        {
+            PopulateProcess();
+        }
+
+
+        private ProcessViewModel _selectedProcess;
+
+        public ProcessViewModel SelectedProcess
+        {
+            get { return _selectedProcess; }
+            set
+            {
+                if (_selectedProcess != value)
+                {
+                    _selectedProcess = value;
+
+                    RaisePropertyChanged(nameof(SelectedProcess));
+                }
+            }
+        }
+
+        private IList<ProcessViewModel> _processList;
+
+        public IList<ProcessViewModel> ProcessList
+        {
+            get
+            {
+                return _processList;
+            }
+            set
+            {
+                if (_processList != value)
+                {
+                    _processList = value;
+                    RaisePropertyChanged(nameof(ProcessList));
+                }
+            }
+        }
+
+        private void PopulateProcess()
+        {
+            ProcessList = System.Diagnostics.Process.GetProcesses().Select(p => new ProcessViewModel
+            {
+                Id = p.Id,
+                Name = p.ProcessName,
+            }).ToList();
+        }
+
+    }
+}
