a2e104093a9c39ef6248daeab8761495217243e6
DerekLoveCC <zhaomingbo1010@126.com>
2020-01-22 23:39:32 +0800

    can inject

--- DotNetMonitor.ManagedInjector/Injector.cpp
+++ DotNetMonitor.ManagedInjector/Injector.cpp
@@ -22,152 +22,154 @@ static HHOOK _messageHookHandle;
 //-----------------------------------------------------------------------------
 void Injector::Launch(IntPtr windowHandle, InjectorData^ injectorData)
 {
-	auto transportDataString = String::Empty;
+    auto transportDataString = String::Empty;
 
-	{
-		auto serializer = gcnew Xml::Serialization::XmlSerializer(InjectorData::typeid);
+    {
+        auto serializer = gcnew Xml::Serialization::XmlSerializer(InjectorData::typeid);
 
-	    StringWriter^ stream = nullptr;
-		try
-		{
-			stream = gcnew StringWriter();
-		    {
-		        serializer->Serialize(stream, injectorData);
-	    		transportDataString = stream->ToString();
-		    }
-		}
-		finally
-		{
-			if (stream != nullptr)
-			{
-				delete stream;
-			}
-		}
-	}
+        StringWriter^ stream = nullptr;
+        try
+        {
+            stream = gcnew StringWriter();
+            {
+                serializer->Serialize(stream, injectorData);
+                transportDataString = stream->ToString();
+            }
+        }
+        finally
+        {
+            if (stream != nullptr)
+            {
+                delete stream;
+            }
+        }
+    }
 
-	const pin_ptr<const wchar_t> acmLocal = PtrToStringChars(transportDataString);
+    const pin_ptr<const wchar_t> acmLocal = PtrToStringChars(transportDataString);
 
-	HINSTANCE hinstDLL;	
+    HINSTANCE hinstDLL;
 
-	if (::GetModuleHandleEx(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS, (LPCTSTR)&MessageHookProc, &hinstDLL))
-	{
-		LogMessage("GetModuleHandleEx successful", true);
-		DWORD processID = 0;
-		DWORD threadID = GetWindowThreadProcessId((HWND)windowHandle.ToPointer(), &processID);
+    if (::GetModuleHandleEx(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS, (LPCTSTR)& MessageHookProc, &hinstDLL))
+    {
+        LogMessage("GetModuleHandleEx successful", true);
+        DWORD processID = 0;
+        DWORD threadID = GetWindowThreadProcessId((HWND)windowHandle.ToPointer(), &processID);
 
-		if (processID)
-		{
-			LogMessage("Got process id", true);
-			HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processID);
-			if (hProcess)
-			{
-				LogMessage("Got process handle", true);
-				int buffLen = (transportDataString->Length + 1) * sizeof(wchar_t);
-				void* acmRemote = VirtualAllocEx(hProcess, nullptr, buffLen, MEM_COMMIT, PAGE_READWRITE);
+        if (processID)
+        {
+            LogMessage("Got process id", true);
+            HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processID);
+            if (hProcess)
+            {
+                LogMessage("Got process handle", true);
+                int buffLen = (transportDataString->Length + 1) * sizeof(wchar_t);
+                void* acmRemote = VirtualAllocEx(hProcess, nullptr, buffLen, MEM_COMMIT, PAGE_READWRITE);
 
-				if (acmRemote)
-				{
-					LogMessage("VirtualAllocEx successful", true);
-					WriteProcessMemory(hProcess, acmRemote, acmLocal, buffLen, nullptr);
-				
-					_messageHookHandle = ::SetWindowsHookEx(WH_CALLWNDPROC, &MessageHookProc, hinstDLL, threadID);
+                if (acmRemote)
+                {
+                    LogMessage("VirtualAllocEx successful", true);
+                    WriteProcessMemory(hProcess, acmRemote, acmLocal, buffLen, nullptr);
 
-					if (_messageHookHandle)
-					{
-						LogMessage("SetWindowsHookEx successful", true);
-						SendMessage((HWND)windowHandle.ToPointer(), WM_GOBABYGO, (WPARAM)acmRemote, 0);
-						UnhookWindowsHookEx(_messageHookHandle);
-					}
+                    _messageHookHandle = ::SetWindowsHookEx(WH_CALLWNDPROC, &MessageHookProc, hinstDLL, threadID);
 
-					VirtualFreeEx(hProcess, acmRemote, 0, MEM_RELEASE);
-				}
+                    if (_messageHookHandle)
+                    {
+                        LogMessage("SetWindowsHookEx successful", true);
+                        SendMessage((HWND)windowHandle.ToPointer(), WM_GOBABYGO, (WPARAM)acmRemote, 0);
+                        UnhookWindowsHookEx(_messageHookHandle);
+                    }
 
-				CloseHandle(hProcess);
-			}
-		}
-		FreeLibrary(hinstDLL);
-	}
+                    VirtualFreeEx(hProcess, acmRemote, 0, MEM_RELEASE);
+                }
+
+                CloseHandle(hProcess);
+            }
+        }
+        FreeLibrary(hinstDLL);
+    }
 }
 
 void Injector::LogMessage(String^ message, bool append)
-{	            
-	String ^ applicationDataPath = Environment::GetFolderPath(Environment::SpecialFolder::ApplicationData);
-	applicationDataPath += "\\DotnetMonitor";
+{
+    String^ applicationDataPath = Environment::GetFolderPath(Environment::SpecialFolder::ApplicationData);
+    applicationDataPath += "\\DotnetMonitor";
 
-	if (!Directory::Exists(applicationDataPath))
-	{
-		Directory::CreateDirectory(applicationDataPath);
-	}
+    if (!Directory::Exists(applicationDataPath))
+    {
+        Directory::CreateDirectory(applicationDataPath);
+    }
 
-	String ^ pathname = applicationDataPath + "\\DotnetMonitorLog.txt";
+    String^ pathname = applicationDataPath + "\\DotnetMonitorLog.txt";
 
-	if (!append)    
-	{    
-		File::Delete(pathname);        
-	}
+    if (!append)
+    {
+        File::Delete(pathname);
+    }
 
-	FileInfo ^ fi = gcnew FileInfo(pathname);
-	            
-	StreamWriter ^ sw = fi->AppendText();   
-	sw->WriteLine(DateTime::Now.ToString("MM/dd/yyyy HH:mm:ss") + " : " + message);
-	sw->Close();
+    FileInfo^ fi = gcnew FileInfo(pathname);
+
+    StreamWriter^ sw = fi->AppendText();
+    sw->WriteLine(DateTime::Now.ToString("MM/dd/yyyy HH:mm:ss") + " : " + message);
+    sw->Close();
 }
 
-__declspec(dllexport) 
+__declspec(dllexport)
 LRESULT __stdcall MessageHookProc(int nCode, WPARAM wparam, LPARAM lparam)
 {
-	if (nCode == HC_ACTION)
-	{
-		const auto msg = (CWPSTRUCT*)lparam;
+    if (nCode == HC_ACTION)
+    {
+        const auto msg = (CWPSTRUCT*)lparam;
 
-		if (msg != nullptr 
-			&& msg->message == WM_GOBABYGO)
-		{
-			Diagnostics::Debug::WriteLine("Got WM_GOBABYGO message");
+        if (msg != nullptr
+            && msg->message == WM_GOBABYGO)
+        {
+            Diagnostics::Debug::WriteLine("Got WM_GOBABYGO message");
 
-			const auto acmRemote = (wchar_t*)msg->wParam;
+            const auto acmRemote = (wchar_t*)msg->wParam;
 
-			const auto acmLocal = gcnew String(acmRemote);
-			Diagnostics::Debug::WriteLine(String::Format("acmLocal = {0}", acmLocal));
+            const auto acmLocal = gcnew String(acmRemote);
+            Diagnostics::Debug::WriteLine(String::Format("acmLocal = {0}", acmLocal));
 
-			auto serializer = gcnew Xml::Serialization::XmlSerializer(InjectorData::typeid);
+            auto serializer = gcnew Xml::Serialization::XmlSerializer(InjectorData::typeid);
 
-			const auto stringReader = gcnew StringReader(acmLocal);
-			const auto injectorData = static_cast<InjectorData^>(serializer->Deserialize(stringReader));
+            const auto stringReader = gcnew StringReader(acmLocal);
+            const auto injectorData = static_cast<InjectorData^>(serializer->Deserialize(stringReader));
 
-			Diagnostics::Debug::WriteLine(String::Format("About to load assembly {0}", injectorData->AssemblyName));
-			auto assembly = Reflection::Assembly::LoadFile(injectorData->AssemblyName);
+            Diagnostics::Debug::WriteLine(String::Format("About to load assembly {0}", injectorData->AssemblyName));
+            auto assembly = Reflection::Assembly::LoadFile(injectorData->AssemblyName);
 
-			if (assembly != nullptr)
-			{
-				Diagnostics::Debug::WriteLine(String::Format("About to load type {0}", injectorData->ClassName));
-				auto type = assembly->GetType(injectorData->ClassName);
-				if (type != nullptr)
-				{
-					Diagnostics::Debug::WriteLine(String::Format("Just loaded the type {0}", injectorData->ClassName));
+            if (assembly != nullptr)
+            {
+                Diagnostics::Debug::WriteLine(String::Format("About to load type {0}", injectorData->ClassName));
+                auto type = assembly->GetType(injectorData->ClassName);
+                if (type != nullptr)
+                {
+                    Diagnostics::Debug::WriteLine(String::Format("Just loaded the type {0}", injectorData->ClassName));
 
-					Diagnostics::Debug::WriteLine(String::Format("About to get method info for {0}", injectorData->MethodName));
-					auto methodInfo = type->GetMethod(injectorData->MethodName, Reflection::BindingFlags::Static | Reflection::BindingFlags::Public);
+                    Diagnostics::Debug::WriteLine(String::Format("About to get method info for {0}", injectorData->MethodName));
+                    auto methodInfo = type->GetMethod(injectorData->MethodName, Reflection::BindingFlags::Static | Reflection::BindingFlags::Public);
 
-					if (methodInfo != nullptr)
-					{
-						Diagnostics::Debug::WriteLine(String::Format("Just got method info for {0}", injectorData->MethodName));
+                    if (methodInfo != nullptr)
+                    {
+                        Diagnostics::Debug::WriteLine(String::Format("Just got method info for {0}", injectorData->MethodName));
 
-						Diagnostics::Debug::WriteLine(String::Format("About to invoke {0} on type {1}", methodInfo->Name, injectorData->ClassName));
-						auto args = gcnew array<Object^>(1);
-						args[0] = injectorData->SettingsFile;
-						auto returnValue = methodInfo->Invoke(nullptr, args);
+                        Diagnostics::Debug::WriteLine(String::Format("About to invoke {0} on type {1}", methodInfo->Name, injectorData->ClassName));
+                        /*auto args = gcnew array<Object^>(1);
+                        args[0] = injectorData->SettingsFile;
+                        auto returnValue = methodInfo->Invoke(nullptr, args);*/
 
-						if (nullptr == returnValue)
-						{
-							returnValue = "NULL";
-						}
-						Diagnostics::Debug::WriteLine(String::Format("Return value of {0} on type {1} is {2}", methodInfo->Name, injectorData->ClassName, returnValue));
-					}
-				}
-			}
-		}
-	}
+                        auto args = gcnew array<Object^>(0);
+                        auto returnValue = methodInfo->Invoke(nullptr, args);
+                        if (nullptr == returnValue)
+                        {
+                            returnValue = "NULL";
+                        }
+                        Diagnostics::Debug::WriteLine(String::Format("Return value of {0} on type {1} is {2}", methodInfo->Name, injectorData->ClassName, returnValue));
+                    }
+                }
+            }
+        }
+    }
 
-	return CallNextHookEx(_messageHookHandle, nCode, wparam, lparam);
+    return CallNextHookEx(_messageHookHandle, nCode, wparam, lparam);
 }
