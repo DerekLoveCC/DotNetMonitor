e11041d790e011a027da1dfd261fd7d5a6a7e54d
DerekLoveCC <zhaomingbo1010@126.com>
2020-01-23 16:12:04 +0800

    add template selector

---
+++ DotNetMonitor.UI/TemplateSelector/DockingManagerLayoutItemSelector.cs
@@ -0,0 +1,20 @@
+﻿using DotNetMonitor.UI.ViewModels;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DotNetMonitor.UI.TemplateSelector
+{
+    public class DockingManagerLayoutItemSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item is ProcessDetail)
+            {
+                var foundTempalte = Application.Current.MainWindow.TryFindResource("ProcessDocumentPaneTemplate") as DataTemplate;
+                return foundTempalte;
+            }
+
+            return base.SelectTemplate(item, container);
+        }
+    }
+}
