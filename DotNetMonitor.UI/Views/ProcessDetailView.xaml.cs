b4a860b33c1bce79240097cb3d4d70ae9969ea79
zhaomingob <zhaomingbo@126.comm>
2025-04-24 22:10:09 +0800

    metro window

--- DotNetMonitor.UI/Views/ProcessDetailView.xaml.cs
+++ DotNetMonitor.UI/Views/ProcessDetailView.xaml.cs
@@ -1,27 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using MahApps.Metro.Controls;
 
-namespace DotNetMonitor.UI.Views
+namespace DotNetMonitor.UI.Views;
+
+/// <summary>
+/// Interaction logic for ProcessDetailView.xaml
+/// </summary>
+public partial class ProcessDetailView : MetroWindow
 {
-    /// <summary>
-    /// Interaction logic for ProcessDetailView.xaml
-    /// </summary>
-    public partial class ProcessDetailView : Window
+    public ProcessDetailView()
     {
-        public ProcessDetailView()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
